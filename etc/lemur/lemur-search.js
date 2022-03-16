26952f4e723d00cde9fea99c280b05aa13ad8e90
Thomas Cardwell <79612290+tdgcardwell@users.noreply.github.com>
2022-03-16 15:59:59 -0400

    fixed lemur search: - if you have adtl links - they need title attributes -and they need holes punched in the search logic, so they don't disappear.

--- etc/lemur/lemur-search.js
+++ etc/lemur/lemur-search.js
@@ -2,7 +2,7 @@
 function captionSearch() {
 
   // get input from search bar value
-  let searchString = document.getElementById('searchbar').value
+  let searchString = document.getElementById('searchbar').value;
 
   // convert to lower case, so that search is not case sensitive
   searchString=searchString.toLowerCase();
@@ -10,11 +10,19 @@ function captionSearch() {
   // get each of the a tags, that hold the title trribute with the captions
   let images = document.getElementsByTagName('a');
 
+
+
   // FOR each caption
   for (i = 0; i < images.length; i++) {
 
+    // so that the links at the top don't mess things up.
+    if (images[i].getAttribute('title').includes("header-link")){
+
+      // display as normal
+      images[i].style.display="block";
+
     // IF searchString to lowercase is NOT included within caption
-    if (!images[i].getAttribute('title').toLowerCase().includes(searchString)) {
+    } else if (!images[i].getAttribute('title').toLowerCase().includes(searchString)) {
 
       // set to display none
       images[i].style.display="none";
